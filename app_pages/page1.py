9b332a3dcee5a1319f7cb69d1eec9eb13d2fc6f9
Dennis Kwok <motokwok@gmail.com>
2025-08-26 16:36:28 +0100

    app with multiple pages

--- app_pages/page1.py
+++ app_pages/page1.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+def page1_body():
+    st.title("Page 1")
+    st.write("Welcome to Page 1")
--- app_pages/page2.py
+++ app_pages/page2.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+def page2_body():
+    st.title("Page 2")
+    st.write("Welcome to Page 2")
